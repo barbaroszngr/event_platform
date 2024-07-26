cf6ca295a088940daab1938ffe5b618c77b4000a
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-26 19:46:08 +0300

    clerk auth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -22,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={poppins.className}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
